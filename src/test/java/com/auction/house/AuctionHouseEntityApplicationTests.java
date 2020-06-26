4ea2d07ad3db93b40f324bc743edcb2d4aafc0fc
Jérémy Somsouk <jeremy.somsouk@manomano.com>
2020-06-26 10:15:35 +0200

    [AH] Initial setup

---
+++ src/test/java/com/auction/house/AuctionHouseEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auction.house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionHouseEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
